f30c376b408fbdc6be3e8f297302d633b149db1d
baldgenie <siddhartha.kapoor@gmail.com>
2023-05-01 23:56:17 +0545

    commit

--- components/Project.js
+++ components/Project.js
@@ -16,7 +16,7 @@ function Project() {
       <div className="scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A] relative w-full flex overflow-y-hidden snap-x snap-mandatory z-20">
         {projects.map((project, i) => {
           return (
-            <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen ">
+            <div key={i} className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen ">
               <motion.img
                 initial={{
                   y: -300,
