4da0cd2181b8c09462d87f63ea1d5afd994eac1a
baldgenie <siddhartha.kapoor@gmail.com>
2023-05-01 18:09:38 +0545

    commit

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { SocialIcon } from "react-social-icons";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 function Header({}) {
   return (
@@ -42,39 +43,41 @@ function Header({}) {
           fgColor="gray"
           bgColor="transparent"
         />
-          <SocialIcon
+        <SocialIcon
           url="https://github.com/bhola2266"
           fgColor="gray"
           bgColor="transparent"
         />
       </motion.div>
 
-      <motion.div
-        initial={{
-          x: 500,
-          opacity: 0,
-          scale: 0.5,
-        }}
-        animate={{
-          x: 0,
-          opacity: 1,
-          scale: 1,
-        }}
-        transition={{
-          duration: 1,
-        }}
-        className="flex items-center  cursor-pointer"
-      >
-        <SocialIcon
-          network="email"
-          className="cursor-pointer"
-          fgColor="gray"
-          bgColor="transparent"
-        />
-        <p className="uppercase hidden md:inline-flex text-sm text-gray-400 ">
-          Get in touch
-        </p>
-      </motion.div>
+      <Link href="#contact">
+        <motion.div
+          initial={{
+            x: 500,
+            opacity: 0,
+            scale: 0.5,
+          }}
+          animate={{
+            x: 0,
+            opacity: 1,
+            scale: 1,
+          }}
+          transition={{
+            duration: 1,
+          }}
+          className="flex items-center  cursor-pointer"
+        >
+          <SocialIcon
+            network="email"
+            className="cursor-pointer"
+            fgColor="gray"
+            bgColor="transparent"
+          />
+          <p className="uppercase hidden md:inline-flex text-sm text-gray-400 ">
+            Get in touch
+          </p>
+        </motion.div>
+      </Link>
     </header>
   );
 }
