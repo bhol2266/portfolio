fd76128aba382de1a717350bd17d2279b6585126
baldgenie <siddhartha.kapoor@gmail.com>
2023-05-09 16:13:13 +0545

    commit

--- components/WorkExperience.js
+++ components/WorkExperience.js
@@ -17,7 +17,7 @@ export default function WorkExperience() {
       </h3>
 
       
-      <div className="scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A] w-full flex space-x-5 lg:space-x-10 xl:space-x-[80px] overflow-x-scroll p-10 snap-x snap-mandatory">
+      <div className=" scrollbar-track-gray-400/20 scrollbar-thin scrollbar-thumb-[#F7AB0A] w-full flex space-x-5 lg:space-x-10 xl:space-x-[80px] overflow-x-scroll p-10 snap-x snap-mandatory">
         <ExperienceCard />
         <ExperienceCard />
         <ExperienceCard />
