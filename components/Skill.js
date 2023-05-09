567d22cd0f29ba4f8e7e6c558029e14a1d934bca
baldgenie <siddhartha.kapoor@gmail.com>
2023-05-09 16:09:42 +0545

    commit

--- components/Skill.js
+++ components/Skill.js
@@ -8,7 +8,7 @@ export default function Skill({ skill }) {
   return (
     <div className="group relative flex cursor-pointer ">
       <motion.img
-        initial={{ x: -200, opacity: 0 }}
+        initial={{ x: -100, opacity: 0 }}
         transition={{ duration: 1 }}
         whileInView={{ opacity: 1, x: 0 }}
         src={urlFor(skill?.image).url()}
