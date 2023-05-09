6003a55b1d25d98036fa5210105132f6f9a72a1c
baldgenie <siddhartha.kapoor@gmail.com>
2023-05-09 15:04:39 +0545

    commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+   
+    NEXT_PUBLIC_SANITY_DATASET: 'production', 
+    NEXT_PUBLIC_SANITY_PROJECT_ID: 'pffpqksx',
+    NEXT_PUBLIC_BASE_URL:'https://bhola.sanity.studio/',
+
+  },
 }
 
 module.exports = nextConfig
