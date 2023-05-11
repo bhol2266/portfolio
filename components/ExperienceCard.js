b39c2cc2f0b720a9bb71c62fc1e41c4c8f434f56
baldgenie <siddhartha.kapoor@gmail.com>
2023-05-12 02:27:28 +0545

    commit

--- components/ExperienceCard.js
+++ components/ExperienceCard.js
@@ -4,7 +4,7 @@ import { urlFor } from "../sanity";
 
 export default function ExperienceCard({ experience }) {
   return (
-    <article className="relative top-10 md:top-4 flex flex-col rounded-xl items-center space-y-2 flex-shrink-0 w-screen lg:w-[600px] xl:w-[700px] snap-center bg-[#292929] p-3 sm:p-4 lg:p-6 2xl:p-10  hover:opacity-100 lg:opacity-40 duration-200 translate-opacity overflow-hidden">
+    <article className="relative top-10 md:top-4 flex flex-col rounded-xl items-center space-y-2 flex-shrink-0 w-screen sm:w-full lg:w-[650px] xl:w-[800px] 2xl:w-[900px] snap-center bg-[#292929] p-3 sm:p-4 md:px-6 lg:p-8 2xl:p-10 2xl:pt-4  hover:opacity-100 lg:opacity-40 duration-200 translate-opacity overflow-hidden">
       <motion.img
         initial={{
           y: -100,
@@ -16,7 +16,7 @@ export default function ExperienceCard({ experience }) {
           y: 0,
         }}
         viewport={{ once: true }}
-        className="w-32 h-32 rounded-full xl:w-[200px] object-cover object-top"
+        className="w-32 h-32 xl:w-36 object-cover "
         src={urlFor(experience?.companyImage).url()}
         alt=""
       />
@@ -45,7 +45,7 @@ export default function ExperienceCard({ experience }) {
           Present
         </p>
 
-        <ul className=" px-4 list-disc space-y-1 xl:space-y-4 ml-2 md:ml-5 text-xs">
+        <ul className=" px-4 list-disc space-y-1 xl:space-y-2 ml-2 md:ml-5 text-xs md:text-sm">
           {experience.points.map((point) => {
             return <li key={point}>{point}</li>;
           })}
